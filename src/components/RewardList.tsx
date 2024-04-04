de94f05dd9424ed0a825c1bfb19e677ce106be9e
Robert Kazaryan <rk30@gcloud.ua.es>
2024-04-04 19:39:44 +0200

    Add group and user information to RewardList component

--- src/components/RewardList.tsx
+++ src/components/RewardList.tsx
@@ -1,17 +1,21 @@
 import { IonAccordion, IonAccordionGroup, IonBackButton, IonButton, IonButtons, IonCardContent, IonCol, IonContent, IonDatetime, IonDatetimeButton, IonHeader, IonIcon, IonInput, IonItem, IonItemDivider, IonItemGroup, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonLoading, IonModal, IonNote, IonPage, IonProgressBar, IonRow, IonSearchbar, IonSelect, IonSelectOption, IonTitle, IonToggle, IonToolbar, SearchbarInputEventDetail, useIonViewWillEnter } from '@ionic/react';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
-import { ValidationErrorResponse, Reward, RewardApi } from '../api';
+import { ValidationErrorResponse, Reward, RewardApi, Group, GroupApi, User } from '../api';
 import { useApp } from '../context/AppContext';
 import RewardInfo from './RewardInfo';
+import { filterSharp } from 'ionicons/icons';
+import { getIcon } from '../constants/constants';
 
 const RewardList: React.FC = () => {
     const navigate = useHistory();
-    const {apiConf} = useApp();
+    const {apiConf, authEmail} = useApp();
     const [rewardList, setRewardList] = useState<Reward[]>();
     const [rewardSearchList, setSearchRewardList] = useState<Reward[]>();
     const [loading, setLoading] = useState<boolean>(true);
-    
+    const [group, setGroup] = useState<Group>();
+    const [currentUser, setCurrentUser] = useState<User>();
+
     useIonViewWillEnter(() => {
         load();
         console.log('Group reward list ionViewDidEnter event fired');
@@ -21,14 +25,20 @@ const RewardList: React.FC = () => {
         setLoading(true);
         
         try {
-            console.log('Senging recuest');
+            console.log('Senging request');
+            var api = new GroupApi(apiConf);
+            var response = await api.getGroup();
+            setGroup(response.data);
+            // Current user is the creator of the group or the couple
+            var user = response.data.creator?.email == authEmail ? response.data.creator : response.data.couple;
+            setCurrentUser(user)
             
-            var api = new RewardApi(apiConf);           
-            var response = await api.getGroupRewardList();
-
-            setRewardList(response.data);
-            setSearchRewardList(response.data);
-            console.log(response);
+            console.log('Senging request');
+            var apiR = new RewardApi(apiConf);           
+            var responseR = await apiR.getGroupRewardList();
+            setRewardList(responseR.data);
+            setSearchRewardList(responseR.data);
+            console.log(responseR);
         } catch (error: any) {
             console.log("Key:" +apiConf!.accessToken);
 
@@ -129,6 +139,9 @@ const RewardList: React.FC = () => {
             case 'redeem':
                 setSearchRewardList(rewardList!.map(reward => {
                     if (reward.id === id) {
+                        if (group?.conf_r_valiadte === false){
+                            reward.validate = true;
+                        }
                         reward.redeem = true;
                         return reward;
                     } else {
@@ -169,26 +182,34 @@ const RewardList: React.FC = () => {
                     </IonButtons>
                     <IonTitle>Reward List</IonTitle>
                 </IonToolbar>
+                <IonToolbar>
+                    <IonSearchbar debounce={1000} onIonInput={(ev) => handleInput(ev)} ></IonSearchbar>
+                    <IonButton slot='end' fill='clear' style={{'--padding-start': '0.2em'}}><IonIcon icon={filterSharp}></IonIcon></IonButton>
+                </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding">
                 <IonLoading className="custom-loading" isOpen={loading} message="Loading" spinner="circles" />
-                <IonSearchbar debounce={1000} onIonInput={(ev) => handleInput(ev)}></IonSearchbar>
                 {rewardSearchList?.map((reward) => {
                     return (
                         <IonAccordionGroup expand="inset" key={reward.id}>
                             <IonAccordion>
-                                <IonItem slot="header" color="">
+                                <IonItem slot="header" color={reward.user?.email == authEmail ? "secondary" : "light"}>
                                     <IonLabel>
                                         <div className="ion-padding-bottom">
                                             <h3>{reward.title}</h3>
-                                            <small>Cost: {reward.cost} </small><br/>
+                                            <small>Cost: {reward.cost} <IonIcon src={getIcon(group?.points_icon!)}></IonIcon></small><br/>
                                             <small>Expire: {reward.expire_at}</small>
                                         </div>
-                                        <IonProgressBar value={5172/(reward.cost && reward.cost != 0 ? reward.cost : 1)}></IonProgressBar>
+                                        {currentUser?.email == reward.user?.email ?
+                                            <IonProgressBar 
+                                                value={currentUser?.points!/(reward.cost && reward.cost != 0 ? reward.cost : 1)}
+                                                color={'danger'}
+                                            ></IonProgressBar>
+                                        : ''}
                                     </IonLabel>
                                 </IonItem>
                                 <div slot="content">
-                                    <RewardInfo reward={reward} remove={remove} redeem={redeem} validate={validate}/>
+                                    <RewardInfo group={group!} reward={reward} remove={remove} redeem={redeem} validate={validate}/>
                                 </div>
                             </IonAccordion>
                         </IonAccordionGroup>
