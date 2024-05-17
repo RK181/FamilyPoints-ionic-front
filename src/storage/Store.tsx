87447b79bd667fba613fe557a89e5ef92904b1aa
Robert Kazaryan <rk30@gcloud.ua.es>
2024-05-17 19:58:30 +0200

    Remove commented out code and unused functions in Store.tsx

--- src/storage/Store.tsx
+++ src/storage/Store.tsx
@@ -1,18 +1,5 @@
 import { Storage, Drivers } from "@ionic/storage";
 
-//let storage: Storage;
-/*
-export const CreateStore = (name: string) => {
-
-    storage = new Storage({
-        
-        name,
-        driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
-    });
-
-    storage.create();
-}*/
-
 export const ConnectDB = async (name: string) => {
 
     const storage = new Storage({
@@ -23,16 +10,3 @@ export const ConnectDB = async (name: string) => {
     storage.create();
     return storage;
 }
-
-/*
-export const storeSet = (key: string, val: string) => {
-
-    storage.set(key, val);
-}
-
-export const storeGet = async (key: string) => {
-
-    const val = await storage.get(key);
-    return val;
-}
-*/
